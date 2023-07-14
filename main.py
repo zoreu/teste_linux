6b6ad42d18fa236193132439826ed65aa7c632af
Joel Silva <53149347+zoreu@users.noreply.github.com>
2023-07-14 17:31:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# import requests
-# r = requests.get('https://github.com/zoreu/update_oneplay/raw/main/update.txt')
-# codigo = r.text
-# # print(codigo)
-# with open('joel_teste.txt', 'w') as f:
-#     f.write(codigo)
-print('teste de executabel')
+import requests
+r = requests.get('https://github.com/zoreu/update_oneplay/raw/main/update.txt')
+codigo = r.text
+# print(codigo)
+with open('exemplo_gerado.txt', 'w') as f:
+    f.write(codigo)
+print('arquivo gerado com sucesso!')
