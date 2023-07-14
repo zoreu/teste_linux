b91c9d99ef9ed7303702790b81e5eba77eaf231d
Joel Silva <53149347+zoreu@users.noreply.github.com>
2023-07-14 17:27:19 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# import requests
+# r = requests.get('https://github.com/zoreu/update_oneplay/raw/main/update.txt')
+# codigo = r.text
+# # print(codigo)
+# with open('joel_teste.txt', 'w') as f:
+#     f.write(codigo)
+print('teste de executabel')
